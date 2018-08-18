1c07589adef24337d75102b6fc302af154a88e98
d-car <danielscarstensen@gmail.com>
2018-08-17 23:08:35 -0500

    table of data from database printing to console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+//create connection info for db
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -8,4 +9,33 @@ var connection = mysql.createConnection({
     password: "password",
     database: "products"
 })
+
+//connect to db and run function if all good, throw err if not
+function run() {
+    connection.query("SELECT * FROM items", function(err, res) {
+        if (err) {
+            throw err
+        } else {
+            console.log("-----------------------------------------------------------------------\n")
+            console.log("                        Welcome to Bamazon!\n")
+            console.log("-----------------------------------------------------------------------\n")
+            console.log("                        Browse items below\n")
+            console.log("                --------------------------------------")
+
+            for (var i = 0; i < res.length; i++) {
+                console.log("ID: " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + "$"+  res[i].price + " | " + res[i].stock_quantity + "\n");
+            }
+
+        }
+
+        // inquirer.prompt([
+        //     {
+        //         type: "input",
+        //         name: "item_id",
+        //         message: ""
+        //     }
+        // ])
+    })
+};
+
 
--- products.sql
+++ products.sql
@@ -12,13 +12,13 @@ CREATE TABLE items(
     );
     
 INSERT INTO items (product_name, department_name, price, stock_quantity)
-VALUES ("Earthbound (Mother 2) Complete Big Box", "SNES", 875, 1),
-	   ("Chrono Trigger Complete in Box", "SNES", 265.72, 3),
-       ("Super Mario RPG Complete in Box", "SNES", 140.49, 4),
-       ("Donkey Kong Country 2: Diddy's Kong Quest Complete in Box", "SNES", 40, 3),
-       ("The Legend of Zelda: Ocarina of Time Collector's Edition Complete in Box", "N64", 120.64, 2),
-       ("Banjo-Kazooie Complete in Box", "N64", 54.88, 6),
-       ("Super Mario 64 cart only", "N64", 50.47, 8),
-       ("Sonic the Hedgehog 3 Complete in Box", "Genesis", 24.99, 11),
-       ("Vectorman Complete in Box", "Genesis", 15.65, 4),
-       ("Earthworm Jim Complete in Box", "Genesis", 32.69, 2);
+VALUES ("Earthbound (Mother 2) CIB Big Box", "SNES", 875, 1),
+	   ("Chrono Trigger CIB", "SNES", 265.72, 3),
+       ("Super Mario RPG CIB", "SNES", 140.49, 4),
+       ("Donkey Kong Country 2: Diddy's Kong Quest CIB", "SNES", 40, 3),
+       ("The Legend of Zelda: Ocarina of Time Collector's Edition CIB", "N64", 120.64, 2),
+       ("Banjo-Kazooie CIB", "N64", 54.88, 6),
+       ("Super Mario 64 CIB", "N64", 50.47, 8),
+       ("Sonic the Hedgehog 3 CIB", "Genesis", 24.99, 11),
+       ("Vectorman CIB", "Genesis", 15.65, 4),
+       ("Earthworm Jim CIB", "Genesis", 32.69, 2);
